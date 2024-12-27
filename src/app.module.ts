cccaa749a9993f57101b281a77f50b91c91ce1de
Albert Mírek <albert.mirek@bonami.cz>
2024-12-27 13:15:18 +0100

    Add global and specific routes middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { PostsController } from './posts/controllers/posts.controller';
 
 @Module({
   imports: [PostsModule, CommentsModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(PostsController)
+  }
+}
