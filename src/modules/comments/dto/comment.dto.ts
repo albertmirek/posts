afdcf7b992dc831545810fd209bd5f3dcc0494ff
Albert Mírek <albert.mirek@bonami.cz>
2025-01-05 18:00:20 +0100

    Tidy up

--- src/modules/comments/dto/comment.dto.ts
+++ src/modules/comments/dto/comment.dto.ts
@@ -3,9 +3,6 @@ import { z } from 'zod';
 
 export const createCommentSchema = z
   .object({
-    //title: z.string(),
-    //body: z.string(),
-    //authorId: z.number(),
     authorId: z.number(),
     postId: z.number(),
     text: z.string(),
